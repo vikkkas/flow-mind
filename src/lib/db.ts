ff035d87b485acfc75fa7f456746bc1533c6896b
Vikas Prasad <vp030217@gmail.com>
2026-02-28 22:34:58 +0530

    chore: update dependencies and Prisma configuration
    - Updated Prisma dependencies to version 7.4.2 and added PostgreSQL adapter.
    - Modified Prisma schema to use the new client provider.
    - Refactored database client initialization to include PostgreSQL adapter.
    - Updated TypeScript configuration for JSX and included additional type definitions.
    - Added new scripts for Prisma generate and migrate in package.json.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,18 @@
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as {
-    prisma : PrismaClient;
+  prisma: PrismaClient;
+};
+
+function createPrismaClient() {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+  });
+  return new PrismaClient({ adapter });
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
